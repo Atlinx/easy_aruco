bfdfcf44510eccc02500fdbddd03c4e82194e595
Atlinx <atlinuxtm@gmail.com>
2026-05-17 16:08:40 -0400

    Patch create_aruco_marker and create_charuco_board to work with cv2 4.7.x+

--- scripts/create_aruco_marker.py
+++ scripts/create_aruco_marker.py
@@ -51,8 +51,16 @@ def generate_marker(dictionary, marker_id, marker_size, output_path):
     if marker_size_m > A4_SIZE_m[0] or marker_size_m > A4_SIZE_m[1]:
         raise ValueError('given size exceeds A4')
 
-    aruco_dict = cv2.aruco.Dictionary_get(getattr(cv2.aruco, dictionary))
-    imboard = cv2.aruco.drawMarker(aruco_dict, marker_id, marker_size_pixels[0])
+    aruco_dictionary_id = getattr(cv2.aruco, dictionary)
+    if hasattr(cv2.aruco, 'getPredefinedDictionary'):
+        aruco_dict = cv2.aruco.getPredefinedDictionary(aruco_dictionary_id)
+    else:
+        aruco_dict = cv2.aruco.Dictionary_get(aruco_dictionary_id)
+
+    if hasattr(cv2.aruco, 'drawMarker'):
+        imboard = cv2.aruco.drawMarker(aruco_dict, marker_id, marker_size_pixels[0])
+    else:
+        imboard = cv2.aruco.generateImageMarker(aruco_dict, marker_id, marker_size_pixels[0])
 
     f = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
 
